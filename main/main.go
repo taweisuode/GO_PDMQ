afea20b933c97b5696e0339881b3e0b7f401f86d
taweisuode <1064480036zjp@gmail.com>
2019-07-26 13:47:28 +0800

    add:add Makefile

--- main/main.go
+++ main/main.go
@@ -9,22 +9,14 @@ package main
 import (
 	pdmq "GO_PDMQ"
 	"fmt"
-	"go-nsq"
 	"time"
 )
 
 type PDMQHandler struct {
 }
 
-type NsqHandler struct {
-}
-
 func (this *PDMQHandler) HandleMessage(message *pdmq.Message) error {
 	fmt.Printf("[PDMQ CONSUMER] [%+v] get handler msg is [%+v]\n", time.Now().Format("2006-01-02 15:04:05"), string(message.Body))
-	return nil
-}
-func (this *NsqHandler) HandleMessage(message *nsq.Message) error {
-	fmt.Println(string(message.Body))
 	return nil
 }
 func main() {
@@ -36,20 +28,6 @@ func main() {
 	consumer.AddHandler(&PDMQHandler{})
 
 	err = consumer.ConnectToPDMQD("127.0.0.1:9400")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	select {}
-}
-
-func main2() {
-	consumer, err := nsq.NewConsumer("name", "world", nsq.NewConfig())
-	if err != nil {
-		fmt.Println(err)
-	}
-	consumer.AddHandler(&NsqHandler{})
-	err = consumer.ConnectToNSQD("127.0.0.1:4150")
 	if err != nil {
 		fmt.Println(err)
 	}
